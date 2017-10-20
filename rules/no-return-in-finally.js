6935aa8afa684ebf889b3d54a4c654a768560a13
Christopher Joslyn <christopher.joslyn@gmail.com>
2017-10-19 22:31:18 -0400

    Report arrow functions that return without a block statement

--- rules/no-return-in-finally.js
+++ rules/no-return-in-finally.js
@@ -6,9 +6,12 @@ module.exports = {
       CallExpression: function (node) {
         if (isPromise(node)) {
           if (node.callee && node.callee.property && node.callee.property.name === 'finally') {
-            if (node.arguments && node.arguments[0] && node.arguments[0].body && node.arguments[0].body.body) {
-              if (node.arguments[0].body.body.some(function (statement) { return statement.type === 'ReturnStatement' })) {
-                context.report(node.callee.property, 'No return in finally')
+            if (node.arguments && node.arguments[0] && node.arguments[0].body) {
+              if (node.arguments[0].body.body && node.arguments[0].body.body.some((statement) => statement.type === 'ReturnStatement')) {
+                context.report(node.callee.property, 'No return in finally');
+              }
+              if (node.arguments[0].type === 'ArrowFunctionExpression' && node.arguments[0].body.type !== 'BlockStatement') {
+                context.report(node.callee.property, 'No return in finally');
               }
             }
           }
